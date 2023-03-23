2b1d8c5b70ea8062018a265d3d913ef92f06e80e
+ <+>
2023-03-23 11:16:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply =num1*num2;
 
-// Write your code here
+let random = 1;
+Math.random()*10+1;
+
+let num3 = 4;
+let num4 = 1;
+let mod = num3 / num4;
+
+let max = Math.max(17,20,9,7,8,19,13);
+
